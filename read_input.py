c1334b2472add593b34abc1659b3713105682e4f
Phil Boyd <boyd_p@outlook.com>
2022-12-04 09:48:20 +0000

    Fix need to strip input

--- read_input.py
+++ read_input.py
@@ -1,3 +1,4 @@
 def read_input_from_daynum(day_number):
     with open(f"input_files/Day {day_number}.txt", "r") as input_fp:
-        return input_fp.readlines()
+        input_lines = input_fp.readlines()
+        return [line.strip() for line in input_lines]
