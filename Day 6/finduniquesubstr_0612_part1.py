0625a93ea63f1a5eae655bdbf805cf59edc07654
Phil Boyd <boyd_p@outlook.com>
2022-12-06 08:24:51 +0000

    Add day 6 challenge.

---
+++ Day 6/finduniquesubstr_0612_part1.py
@@ -0,0 +1,21 @@
+from read_input import read_input_from_daynum
+
+
+def find_unique_msg_end(msg_length: int, puzzle_input: str):
+    for i in range(len(puzzle_input)):
+        if len(set(puzzle_input[i : i + msg_length])) == msg_length:
+            print(
+                f"Detected unique string {puzzle_input[i : i + msg_length]}. Visible by character {i+msg_length}."
+            )
+            break
+
+
+def main():
+
+    puzzle_input = read_input_from_daynum(6)
+    puzzle_input_str = puzzle_input[0]
+    find_unique_msg_end(4, puzzle_input_str)
+
+
+if __name__ == "__main__":
+    main()
