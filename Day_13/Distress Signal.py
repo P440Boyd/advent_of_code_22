fbfddb692711d39a94b7b99b60ef29de8aa8c6c8
Phil Boyd <boyd_p@outlook.com>
2022-12-16 08:51:05 +0000

    Add day 13

---
+++ Day_13/Distress Signal.py
@@ -0,0 +1,78 @@
+from turtle import left
+from read_input import read_input_from_daynum
+import ast
+
+
+def get_groups(seq):
+    data = []
+    for line in seq:
+        if line == "":
+            yield data
+            data = []
+        else:
+            data.append(line)
+
+
+def normalise_l_r(left, right):
+    if isinstance(left, int):
+        left = [left]
+    elif isinstance(right, int):
+        right = [right]
+    return left, right
+
+
+def evaluate_l_r_bool(left, right):
+    if left < right:
+        return True
+    elif left > right:
+        return False
+    else:
+        return None
+
+
+def correct_order(left, right):
+    if isinstance(left, int) and isinstance(right, int):
+        return evaluate_l_r_bool(left, right)
+    left, right = normalise_l_r(left, right)
+    for l, r in zip(left, right):
+        correct = correct_order(l, r)
+        if isinstance(correct, bool):
+            return correct
+    return evaluate_l_r_bool(len(left), len(right))
+
+
+def part_1(puzzle_input):
+    correct = []
+    for group_index, group in enumerate(get_groups(puzzle_input), start=1):
+        left, right = ast.literal_eval(group[0]), ast.literal_eval(group[1])
+        if correct_order(left, right):
+            correct.append(group_index)
+    print(f"Sum of correct ordered packet indices is {sum(correct)}.")
+
+
+def part_2(puzzle_input):
+    no_space_packets = [ast.literal_eval(line) for line in puzzle_input if line]
+    no_space_packets.extend(([[2]], [[6]]))
+    ordered = False
+    while not ordered:
+        ordered = True
+        for i in range(len(no_space_packets) - 1):
+            if correct_order(no_space_packets[i], no_space_packets[i + 1]) is False:
+                holding_item = no_space_packets[i]
+                no_space_packets[i] = no_space_packets[i + 1]
+                no_space_packets[i + 1] = holding_item
+                ordered = False
+
+    print(
+        f"Indexes two and six multiplied: {(no_space_packets.index([[2]]) + 1) * (no_space_packets.index([[6]]) + 1)}."
+    )
+
+
+def main():
+    puzzle_input = read_input_from_daynum(13)
+    part_1(puzzle_input)
+    part_2(puzzle_input)
+
+
+if __name__ == "__main__":
+    main()
--- Day_8/Treetop_Tree_House_part1.py
+++ Day_8/Treetop_Tree_House_part1.py
@@ -3,8 +3,18 @@ from read_input import read_input_from_daynum
 
 def main():
     puzzle_input = read_input_from_daynum(8)
+    forest_height = len(puzzle_input)
+    forest_width = len(puzzle_input[0])
     matrix = []
-    print("Break")
+    for row in puzzle_input:
+        chars = [0 for _ in row]
+        matrix.append(chars)
+    visible_count = len(puzzle_input[0]) * 2 + len(puzzle_input) * 2 - 4
+    maximum = 0
+    for x in puzzle_input:
+        for y in x:
+
+            print(x, y)
 
 
 if __name__ == "__main__":
